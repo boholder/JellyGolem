3b6b5d29b1f8d13c9726914f3a1bd07f71d27620
boholder <bottleholder@anche.no>
2020-06-01 17:44:06 +0800

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+# you have a download_url and a long_description remain to write
+
+setup(
+    name='jellygolem',
+    version='0.1.0',
+    packages=['jellygolem'],
+    url='https://github.com/boholder/JellyGolem',
+    author='boholder',
+    author_email='boholder@anche.no',
+    description='A bot with emotional simulation that can chat with.',
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Intended Audience :: Developers',
+                 'Development Status :: 2 - Pre-Alpha',
+                 'Topic :: Software Development ']
+)
